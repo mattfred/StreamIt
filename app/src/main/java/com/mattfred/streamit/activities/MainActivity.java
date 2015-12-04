679bb1f66e39188e27d587260fb1efa380c1e923
Matthew Frederick <mrmattfred@gmail.com>
2015-12-03 21:22:21 -0500

    minor cleanup

--- app/src/main/java/com/mattfred/streamit/activities/MainActivity.java
+++ app/src/main/java/com/mattfred/streamit/activities/MainActivity.java
@@ -14,7 +14,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -131,14 +130,6 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return super.onOptionsItemSelected(item);
     }
 
     public void searchClicked(View view) {
