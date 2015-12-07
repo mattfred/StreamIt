f3eb46e4d78341d809b521db25635cb4938d3585
Matthew Frederick <mfrederick@autoloop.com>
2015-12-07 15:48:37 -0500

    bump version and add new add

--- app/src/main/java/com/mattfred/streamit/activities/MovieListActivity.java
+++ app/src/main/java/com/mattfred/streamit/activities/MovieListActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 import com.mattfred.streamit.AnalyticsTrackers;
@@ -25,6 +27,7 @@ public class MovieListActivity extends AppCompatActivity implements AdapterView.
 
     private ListView listView;
     private List<Object> objects;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,12 @@ public class MovieListActivity extends AppCompatActivity implements AdapterView.
         //noinspection ConstantConditions
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mAdView = (AdView) findViewById(R.id.ad_view);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
         objects = Globals.getResults();
 
         listView = (ListView) findViewById(R.id.list_view);
@@ -43,9 +52,28 @@ public class MovieListActivity extends AppCompatActivity implements AdapterView.
     }
 
     @Override
+    protected void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
         trackScreen();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
     }
 
     private void updateDisplay() {
